477fbf6a4b4bee9c728f55fdd99f38f726390f65
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-13 15:04:47 -0400

    new test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-/*
+/**
  * main - puts string
  *
  * Return: success
  *
  * Description: main function that prints a string using the puts func
- * */
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
