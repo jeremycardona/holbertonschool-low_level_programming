7fb341b1e2f84d7af321cdef6155a1c4d15c871c
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-14 16:42:49 -0400

    new test

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - entry point
  *
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" \
-- Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar,\
+	2015-10-19\n", 59);
 	return (1);
 }
