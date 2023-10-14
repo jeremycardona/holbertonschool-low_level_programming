aa507e62c5145a5e29f37a32397867e093785b58
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-14 16:24:42 -0400

    Print a string followed by newline to stderr
    C program that prints exacly a string to the standard
    error.
    
    [Ticket: 1]

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 1 (failure)
+ *
+ * Description: A program that prints a string to the stderr
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
