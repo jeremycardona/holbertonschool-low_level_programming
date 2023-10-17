12fde9f89a26a1a1f2cb36333582dc2aa5cf1779
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-17 14:11:25 -0400

    new test

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar();
 	return (0);
 }
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - nothing yet
+ * Return: sucess
+ * Description: putchar
+ */
+void  _putchar(void);
+
+void _putchar(void)
+{
+	printf("%s\n", "_putchar");
+}
+#endif
