8c51ce06fdf82358321528c59df1321912279a60
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-17 13:39:01 -0400

    Print _putchar
    Print _putchar followed by a new file, return 0.

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: sucess
+ * Description: prin _putchar, followed by newline
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+# READ ME
+This directory might contain solutions for tasks 0 - 11. C nested loops and header files
