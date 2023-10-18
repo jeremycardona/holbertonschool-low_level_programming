462a621248d7abb51bdb076a4e7071c1c777fccc
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-18 10:42:09 -0400

    Print the alphabet in lowercase
    Print the alphabet in lowercase followed by new line.
    ONly using putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void print_alphabet(void);
+/**
+ * main - entry point
+ * Return: sucess
+ * Description: call function print_alphabet.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+/**
+ * print_alphabet - print alphabet
+ * Description: print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		printf("%c", letter);
+	printf("\n");
+}
