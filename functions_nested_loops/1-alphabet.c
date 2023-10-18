f8432b27e0ba8b5796e9ffe654171d8750e0fa5a
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-18 17:28:04 -0400

    new test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,5 @@
-#include <stdio.h>
 #include "main.h"
 
-/**
- * main - entry point
- * Return: sucess
- * Description: call function print_alphabet.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 /**
  * print_alphabet - print alphabet
  * Description: print the alphabet in lowercase
