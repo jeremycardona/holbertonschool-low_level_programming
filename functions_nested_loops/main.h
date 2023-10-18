71453c7d7c2d0ea30fe3993882b7622fd70cf3ae
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-18 16:42:22 -0400

    new test

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,18 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * _putchar - nothing yet
- * Return: sucess
- * Description: putchar
- */
-void print_alphabet(void);
-int  _putchar(void)
-{
-	return (0);
-}
 
 /**
  * print_alphabet - print alphabet
  * Description: print the alphabet in lowercase
  */
+
+void print_alphabet(void);
+
+/**
+ * _putchar - nothing yet
+ * Return: sucess
+ * Description: putchar
+ */
+
+int  _putchar(void)
+{
+	return (0);
+}
+
 #endif
