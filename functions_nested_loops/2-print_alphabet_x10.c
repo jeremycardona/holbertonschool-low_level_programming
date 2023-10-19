35a04ec3c6643004b25a0debd8dace6a33ea93c5
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-19 09:36:26 -0400

    Print alphabet ten times
    Print the alphabet ten times, in lowercase, followed by a
    new line. Using _putchar function.

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * Description: print the alphabet ten times in lowercase.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	short i;
+
+	for (i = 0; i < 10; i++)
+		print_alphabet();
+}
