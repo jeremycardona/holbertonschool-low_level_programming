857386532c00defa96cb203670379fee7b7aa972
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-19 09:39:39 -0400

    fix typo

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * _putchar - nothing yet
- * Return: sucess
- * Description: putchar
- */
 int _putchar(char c);
 void print_alphabet(void);
-void print_alphabet_x10(void);
+void print_alphabet_X10(void);
 
 #endif
