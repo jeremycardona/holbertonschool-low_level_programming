a5bc734a5925c6f939371ccc7d88602d47b49c78
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-21 12:57:38 -0400

    new test

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -31,7 +31,11 @@ void _put_n(int n)
 	if (n > 99)
 	{
 		_putchar('0' + n / 100);
-		_putchar('0' + n / 10);
+		if (n / 10 > 9)
+		{
+			_putchar('0' + (n / 10) / 10);
+			_putchar('0' + n % 10);
+		}
 	}
 	else if (n > 9)
 	{
@@ -83,6 +87,7 @@ void print_less_98(int n)
 		_put_n(n);
 		if (n != 98)
 			_put_comma_space();
+		n *= -1;
 		n++;
 	}
 	_putchar('\n');
