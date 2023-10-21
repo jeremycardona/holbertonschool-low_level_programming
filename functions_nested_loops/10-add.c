c9b148a6a0fecd40b785125616a5ebacf15a9942
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-21 11:45:17 -0400

    fix betty style

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -3,6 +3,8 @@
  * add - add two integers and returns the result
  * Return: the sum of two integers
  * Description: function that adds two integers and returns this sum.
+ * @a: first integer
+ * @b: second integer
  */
 int add(int a, int b)
 {
