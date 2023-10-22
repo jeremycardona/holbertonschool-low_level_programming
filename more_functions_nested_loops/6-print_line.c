bd9da221291164cd4d89b425943db46e84b37a23
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-21 21:48:55 -0400

    wwwww print line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,12 +3,13 @@
  * print_line - draw a straight line
  * Description: draw a straight line
  * in the terminal.
+ * @n: number of times a line is going to print.
  */
 void print_line(int n)
 {
-	while (n--)
-	{
+	do {
 		_putchar('_');
-	}
+		n--;
+	} while (n > 0);
 	_putchar('\n');
 }
