1cd7520c0c0745c6e8d62fdc6e72cde0e1200578
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-21 20:46:22 -0400

    super speedss

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiply two integers
+ * Return: multiplication of param 1 and 2
+ * Description: multiply integer a and b.
+ * @a: integer one
+ * @b: integer two
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
