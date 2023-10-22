a2f9ef6559307b6c05ffb3d7a5a400c2e069ac66
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-22 18:18:38 -0400

    print first 50 fibo nums. last test

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print the first 98 fibonacci numbers
+ * Return: sucess
+ * Description: Print the first 98 fibo nums, starting with 1 and 2.
+ */
+int main(void)
+{
+	int max = 48, term;
+	unsigned long fib, fib_term1 = 1, fib_term2 = 2;
+
+	printf("%lu, %lu, ", fib_term1, fib_term2);
+	for (term = 0; term < max; term++)
+	{
+		fib = fib_term1 + fib_term2;
+		printf("%lu", fib);
+		if (term + 1 < max)
+			printf(", ");
+		fib_term1 = fib_term2;
+		fib_term2 = fib;
+	}
+	printf("\n");
+
+	return (0);
+}
