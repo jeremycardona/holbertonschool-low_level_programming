5e0ab89db042b5986de3681ff12db3f6e57f3946
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-22 16:34:25 -0400

    print first 98 fibo nums. new test 8. fix loop initialization var again

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	int max = 98, term;
+	int max = 100, term;
 	unsigned long fib, fib_term1 = 1, fib_term2 = 2;
 
 	printf("%lu, %lu, ", fib_term1, fib_term2);
-	for (term = 2; term < max; term++)
+	for (term = 1; term < max; term++)
 	{
 		fib = fib_term1 + fib_term2;
 		printf("%lu", fib);
