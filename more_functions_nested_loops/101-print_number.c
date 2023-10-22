0764c682fd776d815fe9295cd50a8e10c1f8c63b
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-22 18:35:52 -0400

    zzz

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/* prototypes here */
+void put_num(int);
+/**
+ * more_numbers - print nummber
+ * Description: print a num
+ * @n: number to print 
+ */
+
+void print_number(int n)
+{
+	put_num(n);
+}
+/**
+ * put_num - print numbers up to 99
+ * description: print numbers up to 99 using
+ * _putchar.
+ * @n: number to be printed.
+ */
+void put_num(int n)
+{
+	if (n > 999)
+		_putchar('0' + n / 1000);
+	if (n > 99)
+		_putchar('0' + (n / 100) % 10);
+	if (n > 9)
+		_putchar('0' + (n / 10) % 10);
+	_putchar('0' + n % 10);
+}
