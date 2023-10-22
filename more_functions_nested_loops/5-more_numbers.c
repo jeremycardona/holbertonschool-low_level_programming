d558457303aa544a7c3e38a17717096d40497ff2
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-21 21:25:17 -0400

    cars

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/* prototypes here */
+void put_num(int);
+/**
+ * more_numbers - print nummber 0 to 14, 10 times
+ * Description: print 0 to 14, 10 times.
+ */
+
+void more_numbers(void)
+{
+	int num = 0, times = 10;
+
+	while (times--)
+	{
+		while (num < 15)
+			put_num(num);
+		_putchar('\n');
+		num = 0;
+	}
+
+}
+/**
+ * put_num - print numbers up to 99
+ * description: print numbers up to 99 using
+ * _putchar.
+ * @n: number to be printed.
+ */
+void put_num(int n)
+{
+	if (n > 9)
+		_putchar('0' + n / 10);
+	_putchar('0' + n % 10);
+}
