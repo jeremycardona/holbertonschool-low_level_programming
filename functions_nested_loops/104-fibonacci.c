98c3fe0efd180c15c86573201efa203458b191c2
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-22 16:18:30 -0400

    print first 98 fibo nums. new test 2

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -9,11 +9,11 @@ int main(void)
 	int max = 98, term;
 	long int fib, fib_term1 = 1, fib_term2 = 2;
 
-	printf("%d, %d, ", fib_term1, fib_term2);
+	printf("%ld, %ld, ", fib_term1, fib_term2);
 	for (term = 1; term < max; term++)
 	{
 		fib = fib_term1 + fib_term2;
-		printf("%d", fib);
+		printf("%ld", fib);
 		if (term + 1 < max)
 			printf(", ");
 		fib_term1 = fib_term2;
