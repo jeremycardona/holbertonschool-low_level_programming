8ef15f7eec7b9d36685e15b6fec86decfa85988b
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-21 21:59:33 -0400

    fix blank space before diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,14 +3,23 @@
  * print_diagonal - draw a diagonal line
  * Description: draw a diagonal line
  * in the terminal.
- * @n: number of times diagonal line is 
+ * @n: number of times diagonal line is
  * going to print.
  */
 void print_diagonal(int n)
 {
+	int blank = 0, temp = blank;
+
 	do {
 		if (n > 0)
-			_putchar('/');
+			_putchar('\\');
+		while (temp > 0)
+		{
+			_char(' ');
+			temp--;
+		}
+		blank++;
+		temp = blank;
 		n--;
 	} while (n > 0);
 	_putchar('\n');
