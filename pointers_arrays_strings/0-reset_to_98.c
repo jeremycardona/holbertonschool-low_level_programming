e2f4f81e3db83dabb0407c8ee78f470dddba22cc
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-24 09:24:02 -0400

    Take pointer to an int, update value to 98
    Function that takes a pointer to an int as parameter and
    updates the value it points to 98
    
    [Ticket: X]
    ~
    ~
    ".git/COMMIT_EDITMSG" 14L, 297C

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Take a pointer to an int, update to 98
+ * Description: Function that takes a pointer to an int as
+ * parameter and updates the value to 98.
+ * @n: pointer value to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
