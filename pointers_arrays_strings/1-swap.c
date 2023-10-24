b10406dc93009f605b1c625740f173572c31a748
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-24 09:30:31 -0400

    Swap the values of two integers
    Swap the values of two integers.
    
    [Ticket: X]
    ~
    ~
    ".git/COMMIT_EDITMSG" 14L, 297C

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - Swap the values of two integers
+ * Description: Function that swaps the values of two 
+ * integers.
+ * @a: value one
+ * @b: value two
+ */
+void swap_int(int *a, int *b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
