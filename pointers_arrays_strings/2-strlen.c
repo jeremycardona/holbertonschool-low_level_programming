617eb6fc437852615586f2b80f9d5039f3219e20
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-24 09:50:30 -0400

    Return the length of a string
    Return the legnth of a string. Similar function to strlen.
    
    [Ticket: X]
    ~
    ~
    ".git/COMMIT_EDITMSG" 14L, 297C

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+/**
+ * _strlen - return the length of a string
+ * Description: Function that returns the length of
+ * a string.
+ * Return: Length of s
+ * @s: String.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (s != 0)
+	{
+		int i;
+
+		for (i = 0; *s != '\0'; i++)
+		{
+			len++;
+			s++;
+		}
+	}
+	return (len);
+}
