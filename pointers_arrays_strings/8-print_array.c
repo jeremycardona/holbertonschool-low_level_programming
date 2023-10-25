258ac327d0990360a3ea99863f6257ac89e0cc23
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-25 11:39:50 -0400

    fix betty style

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,11 +2,15 @@
 #include "main.h"
 
 /**
- *
+ * print_array - print array of integers
+ * Description: pritn array of integers.
+ * @a: aray of integers
+ * @n: size of array
  */
 void print_array(int *a, int n)
 {
 	int i;
+
 	if (a != 0)
 	{
 		for (i = 0; i < n; i++)
