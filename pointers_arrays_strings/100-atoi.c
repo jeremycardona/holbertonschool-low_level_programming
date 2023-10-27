b633b8fa69d69b8f25534df15b7ff12423a1ef13
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-26 23:38:19 -0400

    new test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -145,8 +145,6 @@ int get_int(char *s)
 				digit = s[num_index] - '0';
 				if (digit > 0)
 					digit = digit * _pow(10, digit_place);
-				if (result + result + digit - 1 <= INT_MIN)
-					return (INT_MIN);
 				result += digit;
 				digit_place--;
 				s_i++;
