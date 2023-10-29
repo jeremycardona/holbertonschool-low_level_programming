e1895d2084b183f668909484f5008d7251833ee6
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-29 13:29:01 -0400

    sppeeeeedz

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "main.h"
 /**
  * _strcat - concatenate two strings
  * Description: contatenate two strings, append the src to the dest,
@@ -9,15 +11,47 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *cat;
-	int i = 0;
+	int i_dest = 0, i = 0;
+	int dest_len = _strlen(dest);
 
-	while (dest[i] != '\0')
+	while (dest[i_dest])
 	{
-		*(cat++) = dest[i];
-	}
-	i = 0;
-	while 
-	
+		if (src[i] != '\0')
+		{
+			dest[dest_len + i] = src[i];
+			i++;
+			i_dest++;
+		}
+		else
+		{
+			dest[dest_len + i] = '\0';
+			break;
+		}
 
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - return the length of a string
+ * Description: Function that returns the length of
+ * a string.
+ * Return: Length of s
+ * @s: String.
+ */
+int _strlen(char *s)
+{
+        int len = 0;
+
+        if (s != 0)
+        {
+                int i;
+
+                for (i = 0; *s != '\0'; i++)
+                {
+                        len++;
+                        s++;
+                }
+        }
+        return (len);
 }
