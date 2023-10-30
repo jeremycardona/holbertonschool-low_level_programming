e9dd92f2c3f1d27e28b6c0ac76c387910d16e5dd
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-29 23:10:55 -0400

    new test

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,9 @@
-#include "main.h"
+#include <stdio.h>
 /**
- *
+ * cap_string - capitalize each string
+ * Description: meh gtg soon
+ * Return: string capizalized
+ * @str: string to be capitalized
  */
 char *cap_string(char *str)
 {
@@ -15,63 +18,26 @@ char *cap_string(char *str)
 		if (c >= 'a' && c <= 'z')
 			c = (int)c - 32;
 		str[i] = c;
+		
 		while (str[i] != separators[j])
 		{
-			j++;
-			if ('\t' == separators[j])
+			if (str[i] == separators[j])
+			{
+				++i;
+				continue;
+			}
+			else if ('\0' == separators[j])
 			{
 				j = 0;
+				i++;
+			}
+			else
+			{
+				j++;
 				i++;
 			}
 		}
 		i++;
 	}
 	return (str);
-}
-/**
- * _strcmp - compare two strings
- * Description: Compare two strings.
- * Return: if 1st string greater than 2nd string 15, if 1st string less than
- * 2nd string -15, else 0
- * @s1: first string
- * @s2: second string
- */
-
-int _strcmp(char *s1, char *s2)
-{
-        int i = 0;
-
-        while (s1[i] && s2[i])
-        {
-                if (s1[i] > s2[i])
-                        return (13);
-                else if (s1[i] < s2[i])
-                        return (-13);
-                i++;
-        }
-        return (0);
-}
-
-/**
- * _strlen - return the length of a string
- * Description: Function that returns the length of
- * a string.
- * Return: Length of s
- * @s: String.
- */
-int _strlen(char *s)
-{
-        int len = 0;
-
-        if (s != 0)
-        {
-                int i;
-
-                for (i = 0; *s != '\0'; i++)
-                {
-                        len++;
-                        s++;
-                }
-        }
-        return (len);
 }
