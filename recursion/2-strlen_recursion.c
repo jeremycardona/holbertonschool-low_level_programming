f662b250b99fcc6a00d39970f66e4ceb40250556
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-10-31 10:05:23 -0400

    length of a string

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen_recursion - length of a string
+ * Description: return the length of a string.
+ * Return: string length
+ * @s: string
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		s++;
+		len += 1 + _strlen_recursion(s);
+	       	return (len);
+	}
+	return (len);
+}
