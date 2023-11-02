eb1490d5a7e346c322fa7bdc67a5b6c28a1dca68
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-02 14:18:33 -0400

    print program name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print argv
+ * Return: success
+ * @argc: argument count
+ * @argv: argument vector of strings
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
