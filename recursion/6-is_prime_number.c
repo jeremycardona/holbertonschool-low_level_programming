3770747a81b94568f6a2e70c71d6bc56143da715
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-02 09:00:41 -0400

    is prime number?

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,22 +1,34 @@
-/**
- *
- */
+#include "main.h"
 
+int prime_number(int, int);
+/**
+ * is_prime_number - return 1 if integer is a prime
+ * Description: return 1 if int is prime, otherwise return 0
+ * Return: recursive function call
+ * @n: num to evaluate as prime or not
+ */
 int is_prime_number(int n)
 {
-	int i = n / 2;
+	int i = 2;
+	int is_prime = prime_number(n, i);
 
-	if (n == 0 || n == 1)
+	if (is_prime == 1)
+		return (1);
+	return (0);
+}
+/**
+ * prime_number - generate primes
+ */
+int prime_number(int n, int i)
+{
+	if (n <= 1)
 		return (0);
-
-	if (n == i)
-	{
+	else if (n == 2)
 		return (1);
-	}
 	else if (n % i == 0)
-	{
+		return (0);
+	else if (i >= n/2)
 		return (1);
-	}
-	i--;
-	return is_prime_number(n);
+	else
+		return (prime_number(n, i + 1));
 }
