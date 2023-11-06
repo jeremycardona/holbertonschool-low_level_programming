890cf508a9fc7b1034bf59607fc83f20bb92423d
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-06 14:21:26 -0400

    push test

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - return a pointer to a newly allocated space in memory
+ * Description: Return a pointer to a newly allocated space in
+ * memory, hwich contains a cpy of the string given as a param
+ * Return: on success a pointer to th duplacate, null on failure
+ * @str: str to be duplicated
+ */
+char *_strdup(char *str)
+{
+	char *str_cpy;
+
+	if (!str)
+		return (NULL);
+	str_copy = malloc(sizeof(str));
+	if (str_cpy)
+	{
+		strcpy(str_cpy, str);
+		return (str_cpy);
+	}
+	return (NULL);
+}
