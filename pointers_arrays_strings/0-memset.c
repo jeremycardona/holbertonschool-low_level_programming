3afadd3edbedd674fae5b3bf831ef76a9a73b21e
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-05 21:09:56 -0400

    _memset fill n number of bytes of s

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,12 +1,25 @@
+#include <stdio.h>
 /**
- * _memset - something ill try later
- * Description: something ill describe later.
- * Return: something.
- * @s: ba
- * @b: chjar
- * @n: nn
+ * _memset - fill memory with a constant byte
+ * Description: fill the first n bytes of the memory area pointed
+ * by s with a constant byte b.
+ * Return: pointer to memory area s.
+ * @s: string
+ * @b: byte
+ * @n: n bite.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
+	char **pps;
+
+	pps = &s;
+	while (n--)
+	{
+		if (s)
+		{
+			**pps = b;
+			s++;
+		}
+	}
 	return (s);
 }
