c52826c2e01a9b3cd90114abf4a06229404daba5
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-07 09:17:14 -0400

    str_concat, concatenate two strings

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - concatenate two strings
+ * Description: concatenate two strings s1 and s2.
+ * Return: pointer should point to a newly allocated space in mry
+ * @s1: string one
+ * @s2: string two
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s3 = malloc(strlen(s1) + strlen(s2) + 1);
+	
+	if (s3)
+	{
+		strcpy(s3, s1);
+		strcat(s3, s2);
+		return (s3);
+	}	
+	return (NULL);
+
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -3,5 +3,6 @@
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *);
+char *str_concat(char *, char *);
 
 #endif
