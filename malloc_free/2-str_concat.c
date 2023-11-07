c576288d2763c459c2c9802295fbe28e73a389d0
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-07 09:19:04 -0400

    push test

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,13 +10,17 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3 = malloc(strlen(s1) + strlen(s2) + 1);
-	
+
+	if (s1 == NULL)
+		*s1 = "";
+	if (s2 == NULL)
+		*s2 = "";
 	if (s3)
 	{
 		strcpy(s3, s1);
 		strcat(s3, s2);
 		return (s3);
-	}	
+	}
 	return (NULL);
 
 }
