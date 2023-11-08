0b667f2e01c6bde065a9fc4be6d0f29e776a00fe
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-08 10:15:23 -0400

    concat two strings, limit s2 by n

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenate two strings
+ * Description: concatenate two strings, limit s2 by n
+ * Return: a pointer to a newly allocated space in memory
+ * @s1: string one
+ * @s2: string two
+ * @n: first n bytes of s2 to use
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1_len = strlen(s1);
+	unsigned int s2_len = strlen(s2);
+	char *s3;
+
+	if (n >= s2_len)
+		s3 = malloc((s1_len + s2_len) * sizeof(*s3));
+	else
+	{
+		s2_len = n;
+		s3 = malloc((s1_len + s2_len) * sizeof(*s3));
+	}
+	if (!s3)
+	{
+		free(s3);
+		return (NULL);
+	}
+	if (!s1)
+		strcpy(s3, "");
+	if (!s2)
+		strcat(s3, "");
+	if (s1 && s2 && s3)
+	{
+		strcpy(s3, s1);
+		if (s2_len == n)
+			strncat(s3, s2, n);
+		else
+			strcat(s3, s2);
+	}
+	return (s3);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -3,5 +3,6 @@
 
 int _putchar(char);
 void *malloc_checked(unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
 
 #endif
