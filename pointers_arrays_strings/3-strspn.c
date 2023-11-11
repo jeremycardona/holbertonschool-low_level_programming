25b924180086e12c0db3e4450e7da04d97e75ebf
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-11 18:16:31 -0400

    get the length of a prefix substring

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,27 @@
+#include <stdio.h>
 /**
- * _strspn - something ill try later
- * Description: something ill describe later.
- * Return: something.
- * @s: ba
- * @accept: soem
+ * _strspn - get the length of a prefix substrings
+ * Description: function that gets the length of a prefix
+ * substring.
+ * Return: a number of bytes in the initial segment of s which
+ * consist only of bytes from accept
+ * @s: initial segment string
+ * @accept: bytes only from substring
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	return (s);
+	unsigned int num_bytes;
+	int j;
+
+	for (num_bytes = 0; s[num_bytes]; num_bytes++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[num_bytes] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (num_bytes);
 }
