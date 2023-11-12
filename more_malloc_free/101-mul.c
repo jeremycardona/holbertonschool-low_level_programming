221c932ce25289834d65dc3d3ade340da8de6618
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-12 18:06:34 -0400

    push test

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include <ctype.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <stdio.h>
 /**
  * main - multiply two positive numbers
@@ -10,22 +11,38 @@
  */
 int main(int argc, char **argv)
 {
-	unsigned long int num1;
-	unsigned long int num2;
+	unsigned long int *num1;
+	unsigned long int *num2;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
 	
-	if (isalpha(num1) && isalpha(num2))
+	num1 = malloc(sizeof(*num1));
+	if (!num1)
+	{
+		free(num1);
+		printf("Error\n");
+		exit(98);
+	}
+
+	num2 = malloc(sizeof(*num2));
+	if (!num2)
+	{
+		free(num2);
+		printf("Error\n");
+		exit(98);
+	}
+
+	*num1 = atoi(argv[1]);
+	*num2 = atoi(argv[2]);
+	if (isalpha(*num1) && isalpha(*num2))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	printf("%lu\n", num1 * num2);
+	printf("%lu\n", *num1 * *num2);
 	return (0);
 }
