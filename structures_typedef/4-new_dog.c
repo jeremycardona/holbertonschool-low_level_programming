70777b108a6adf29881a3a96fb4bd829dcfdebca
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-13 14:20:37 -0400

    push test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,11 +1,13 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - create anew dog
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
  * Description: function that creates a new dog
+ * Return: dog_t pointer or null if it fails to allocate
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -13,7 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	news = malloc(sizeof(*news));
 	if (!news)
+	{
+		free(news);
 		return (NULL);
+	}
 	news->name = name;
 	news->age = age;
 	news->owner = owner;
