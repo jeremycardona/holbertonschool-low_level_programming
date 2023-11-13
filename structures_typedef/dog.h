752f41cfb7309b7db382d1ba30f29517c07b2911
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-13 13:22:22 -0400

    push test

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-
-struct dog {
+/**
+ * struct dog - structure for dog type
+ * @name: string containing dog name
+ * @age: float for dog age
+ * @owner: stringfor owner name
+ * Description: struct for a dog type
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
