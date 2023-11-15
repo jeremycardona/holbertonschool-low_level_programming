8be7f89cf562dada62b04324bb3c66850d7f5a51
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-15 09:47:24 -0400

    push test

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -9,8 +9,14 @@ void free_dog(dog_t *d)
 {
 	if (d)
 		free(d);
-	if (d->name)
+	else if (d->name)
 		free(d->name);
-	if (d->owner)
+	else if (d->owner)
 		free(d->owner);
+	else
+	{
+		free(d);
+		free(d->name);
+		free(d->owner);
+	}
 }
