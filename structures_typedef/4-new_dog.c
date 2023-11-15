89ced53e9269574344be70a2f61c0ab13c78e0d8
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-15 09:09:45 -0400

    push test

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,10 +1,11 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
 dog_t *create_dog(dog_t *);
-char *create_dog_name(dog_t *);
-char *create_dog_owner(dog_t *);
+char *create_dog_name(dog_t *, int);
+char *create_dog_owner(dog_t *, int);
 /**
  * new_dog - create anew dog
  * @name: dog name
@@ -15,56 +16,32 @@ char *create_dog_owner(dog_t *);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *news;
-	char *n, *o;
+	dog_t *news, *doggy;
 	int l_name = 0, l_owner = 0;
 
 	if (name)
 		l_name = strlen(name);
 	if (owner)
 		l_owner = strlen(owner);
-
-	n = malloc(l_name + 1);
-	if (!n)
-	{
-		free(n);
+	if (l_name + l_owner == 0)
 		return (NULL);
-	}
-	o = malloc(l_owner + 1);
-	if (!o)
+	
+	news = NULL;
+	news = create_dog(news);
+	doggy = news;
+	doggy->name = create_dog_name(news, l_name);
+	doggy->owner = create_dog_owner(news, l_owner);
+	if (!(news->name) || !(news->owner) || !news)
 	{
-		free(o);
-		return (NULL);
-	}
-	news = malloc(sizeof(dog_t));
-	if (!news)
-	{
+		free(news->owner);
+		free(news->name);
 		free(news);
 		return (NULL);
 	}
-	if (create_dog(news))
-		news = create_dog(news);
-	else
-		return (NULL);
-	if (create_dog_owner(news))
-		news->owner = create_dog_owner(news);
-	else
-		return (NULL);
-	if (create_dog_name(news))
-		news->name = create_dog_name(news);
-	else
-		return (NULL);
-	if (news)
-	{
-		news->age = age;
-		news->name = strcpy(n, name);
-		news->owner = strcpy(o, owner);
-		return (news);
-	}
-	free(news->owner);
-	free(news->name);
-	free(news);
-	return (NULL);
+	doggy->age = age;
+	doggy->name = strcpy(news->name, name);
+	doggy->owner = strcpy(news->name, owner);
+	return (news);
 }
 /**
  * create_dog - create memory for struct
@@ -75,7 +52,7 @@ dog_t *new_dog(char *name, float age, char *owner)
  */
 dog_t *create_dog(dog_t *dog)
 {
-	dog = malloc(sizeof(dog_t));
+	dog = malloc(sizeof(*dog));
 	if (!dog)
 	{
 		free(dog);
@@ -89,13 +66,15 @@ dog_t *create_dog(dog_t *dog)
  * and its pointers elements
  * Return: pointer or null
  * @dog: struct of type dog_t
+ * @length: space to allocate
  */
-char *create_dog_name(dog_t *dog)
+char *create_dog_name(dog_t *dog, int length)
 {
-	dog->name = malloc(sizeof(char *));
-	if (dog->name == NULL)
+	dog->name = malloc(sizeof(length + 1));
+	if (dog->name == NULL || !dog)
 	{
 		free(dog->name);
+		free(dog);
 		return (NULL);
 	}
 	return (dog->name);
@@ -106,13 +85,16 @@ char *create_dog_name(dog_t *dog)
  * and its pointers elements
  * Return: pointer or null
  * @dog: struct of type dog_t
+ * @length: space to allocate
  */
-char *create_dog_owner(dog_t *dog)
+char *create_dog_owner(dog_t *dog, int length)
 {
-	dog->owner = malloc(sizeof(char *));
-	if (dog->owner == NULL)
+	dog->owner = malloc(sizeof(length + 1));
+	if (dog->owner == NULL || !(dog->name) || !dog)
 	{
+		free(dog->name);
 		free(dog->owner);
+		free(dog);
 		return (NULL);
 	}
 	return (dog->owner);
