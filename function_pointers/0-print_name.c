47e8ec90a5bcfcb64d86653389084820752a512a
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-16 09:08:05 -0400

    print string

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - prints a name
+ * Description: function that prints a name
+ * @name: string
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
