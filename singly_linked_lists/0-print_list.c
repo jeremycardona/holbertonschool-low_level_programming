ebb8aa003f67a588a7a07298eb56195bf5b5bc85
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-11-27 13:52:03 -0400

    print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_list - print a linked list.
+ * Description: function that prints a linked list
+ * Return: list size
+ * @h: head
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes_count = 0;
+	const list_t *link;
+	link = h;
+
+	while (link)
+	{
+		if (!h->str)
+			printf("[0] (nil)");
+		else
+			printf("%s\n", h->str);
+		link = link->next;
+		nodes_count++;
+	}
+	return (nodes_count);
+}
