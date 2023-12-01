294ec6670ca5ce3fd66f30d1a2722a0d92f190e4
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-12-01 13:56:00 -0400

    push test

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 #include <stdlib.h>
-
 /**
- *
+ * dlistint_len - length of a doubly linked list
+ * Description: function that counts elements in dll
+ * Return: length
+ * @h: doubly list head address
  */
 size_t dlistint_len(const dlistint_t *h)
 {
