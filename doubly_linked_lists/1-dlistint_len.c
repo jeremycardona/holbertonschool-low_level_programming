209fe756f56b9d6b6bdaf540609726e7905d15ea
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-12-01 13:35:35 -0400

    size of doubly ll

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ *
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
