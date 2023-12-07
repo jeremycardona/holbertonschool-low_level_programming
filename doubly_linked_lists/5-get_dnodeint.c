7285afd8c2edbd19a70c1deb38c044a3aca6ab2d
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-12-07 13:34:03 -0400

    return nth node of a dll

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_dnodeint_at_index - get node n-th node of a doubly ll.
+ * Description: function that returns the nth node of a dlistint_t
+ * algorithm: initialize count = 0
+ * looop throught the ll
+ * Return: nth node of dll head->previous, if dne return null.
+ * @head: dll address
+ * @index: index node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while ((head->next))
+	{
+		if (count == index)
+		{
+			return (head);
+		}
+		head = head->next;
+		count++;
+	}
+	return (NULL);
+}
