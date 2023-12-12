2d97e15564fdfb7ccadd6aa9336e45e0735b81c4
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-12-11 20:51:51 -0400

    tamo bine

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * read_textfile - read a text file and print it to POSIX
+ * Description: function that reads a file and pritns it
+ * to the POSIX standard output.
+ * Return: actual number of letters it could read and print
+ * @filename: file name
+ * @letters: number of letters it should read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
