6a3706dfd126f508e6b6729ab561f1248e0e955f
Jeremy Cardona <jeremy.cardona@hotmail.com>
2023-12-21 09:26:50 -0400

    push test

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,19 +8,33 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *t;
+	hash_node_t *ts;
+
+	ts = malloc(sizeof(hash_node_t *));
+	if (!ts)
+	{
+		free(ts);
+		return (NULL);
+	}
 
 	t = malloc(sizeof(hash_table_t *) * (size + 1));
 	if (!t)
 	{
+		free(ts);
 		free(t);
 		return (NULL);
 	}
+	
 	/*
 	*
 	//t.next = node;
 	//node = node->value = 'J';
 	*
 	*/
+	ts->key = "1";
+	ts->value = "one";
+	ts->next = NULL;
 	t->size = size;
+	t->array = &ts;
 	return (t);
 }
