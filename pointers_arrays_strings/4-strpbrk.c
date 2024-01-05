279bff28ce9541ab74417368cb70b87418855e52
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-01-04 23:01:53 -0400

    llo, world

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
 /**
- * _strpbrk - something ill try later
- * Description: something ill describe later.
+ * _strpbrk - search a string for any of a set of bytes
+ * Description: function that locates te first occurence in the string
+ * 's' of any of the bytes in the string accept.
  * Return: something.
- * @s: ba
- * @accept: chjar
+ * @s: string
+ * @accept: any bytes in string 's'
  */
+int _strlen(char *);
+
 char *_strpbrk(char *s, char *accept)
 {
-	return (s);
+	int i;
+	int l = _strlen(s);
+	
+	if (s == 0)
+	{
+		for (i = 0; i < l; i++)
+		{
+			if (s([i][0]) == accept)
+				return (s);
+		}
+	}
+	return (NULL);
 }
